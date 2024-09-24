eae52ef15d814572da3c2c2bf725c224fd5f2b0a
DylanBlanco <dylanblanco1198@gmail.com>
2024-09-24 04:43:08 +0200

    main - Industry Leaders

--- src/components/mainIndustry.vue
+++ src/components/mainIndustry.vue
@@ -1,15 +1,53 @@
 <script>
 export default {
     data() {
-        return {         
+        return {
+            indBox: {
+                who: 'WHO WE ARE',
+                title: 'Industry Leaders',
+                text: 'With all of this expertise and capability comes an unrivalled commitment to customer service.',
+                do: 'We will work hard to understand your needs in order to develop a productive, long-term partnership.',
+                link: 'READ MORE',
+            }
         }
     }
 }
 </script>
 
 <template>
-    <section>
-        
+    <!-- main industry -->
+    <section class="industry">
+        <div class="container">
+            <div class="box">
+                <ul>
+                    <li>
+                        <p class="blue-text">
+                            {{ indBox.who }}
+                        </p>
+                    </li>
+                    <li>
+                        <h2>
+                            {{ indBox.title }}
+                        </h2>
+                    </li>
+                    <li>
+                        <p>
+                            {{ indBox.text }}
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            {{ indBox.do }}
+                        </p>
+                    </li>
+                    <li>
+                        <a href="#">
+                            {{ indBox.link }}
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </section>
 </template>
 
@@ -18,4 +56,16 @@ export default {
 @use '../assets/scss/partials/variables.scss' as *;
 @use '../assets/scss/generalStyle.scss' as*;
 @use '../assets/scss/partials/mainIndustry.scss' as*;
+
+.industry p {
+    color: $textColorMain;
+}
+
+.industry h2 {
+    color: $h2ColorText;
+}
+
+.industry .blue-text {
+    color: $mainTextAColor;
+}
 </style>
