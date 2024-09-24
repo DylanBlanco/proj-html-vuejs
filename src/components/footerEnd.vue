d17ff463c4b5939c7e99e0e9d33888132439a427
DylanBlanco <dylanblanco1198@gmail.com>
2024-09-24 17:34:50 +0200

    footer - footer end

--- src/components/footerEnd.vue
+++ src/components/footerEnd.vue
@@ -2,16 +2,38 @@
 export default {
     data() {
         return {
+            footText: {
+                text: 'Enjoy the low price. We are tracking any intention of privacy.',
+                label: '© 2024 - All rights reserved. Nexgen is maintained by Codings Group.',
+            }
         }
     }
 }
 </script>
 
 <template>
+    <section class="footer-end">
+        <div class="container">
+            <div class="my-row">
+                <div>
+                    <p>
+                        {{ footText.text }}
+                    </p>
+                </div>
+                <div>
+                    <a href="#">
+                        {{ footText.label }}
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <style lang="scss">
 @use '../assets/scss/partials/reset.scss' as*;
 @use '../assets/scss/partials/variables.scss' as *;
 @use '../assets/scss/generalStyle.scss' as*;
+@use '/src/assets/scss/partials/footerEnd.scss' as*;
+
 </style>
